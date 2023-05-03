315332d23de04cd0629c030edffce198338f4b22
iwonaive <szymaniak.iwona11@gmail.com>
2023-05-03 09:53:52 +0200

    added css styles, tryed to add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
